842a7a58e481fe64461cb3e0351668fed6302162
Baaz-Cheema <chrisbahadur1@gmail.com>
2023-12-12 04:31:45 +0500

    bug

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import Carousel from "../Components/HomePage/Carousel/Carousel";
-import MainSection from "../components/HomePage/MainSection";
-import FlashSale from "../components/HomePage/Flash sale/FlashSale";
+import MainSection from "../Components/HomePage/MainSection";
+import FlashSale from "../Components/HomePage/Flash sale/FlashSale";
 
 import { useEffect } from "react";
 export default function HomePage() {
