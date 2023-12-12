4b15454c3243463ab9f4b28ed685f1742cae9e7e
Baaz-Cheema <chrisbahadur1@gmail.com>
2023-12-13 04:06:41 +0500

    added signout functionality

--- src/Components/Account/AccountNavbar.jsx
+++ src/Components/Account/AccountNavbar.jsx
@@ -2,16 +2,24 @@ import styles from './AccountNavbar.module.css'
 import { NavLink } from 'react-router-dom'
 import { useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { AuthSliceActions } from '../../store/AuthSlice';
 
 export default function AccountNavbar() {
+    const dispatch = useDispatch()
     const location = useLocation()
     const isActive = (path) => location.pathname === path; //manually doing what navlink does
-    const username= useSelector(state=>state.auth.username)
+    const username = useSelector(state => state.auth.username)
+    function signout() {
+        dispatch(AuthSliceActions.setLoggedIn('logout'))
+    }
+    
     return (
         <div className={styles.header}>
             <h2 className={styles.heading}>My Account</h2>
-            <p style={{marginTop:'1rem'}}>Welcome back, {username}</p>
-            
+            <p style={{ marginTop: '1rem' }}>Welcome back, {username}</p>
+            <p className={styles.signout} onClick={signout}>Signout</p>
+
             <nav className={styles.navbar}>
                 <ul className={styles.links}>
                     <li className={isActive('/account') ? styles.active : ''}>
@@ -24,7 +32,7 @@ export default function AccountNavbar() {
                         <i className='bx bxs-map'></i> <NavLink to='addresses'>Addresses</NavLink>
                     </li>
                 </ul>
-               
+
             </nav>
         </div>
     )
